32f47afa76d3e782a4c65aef137935995c20d628
Sagarmal kuldiya <163585142+sagarmal15102002@users.noreply.github.com>
2024-08-16 00:27:31 -0700

    Add files via upload

---
+++ tryBook/script.js
@@ -0,0 +1,8 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+});
+
+
+document.getElementsByTagName(section).addEventListener('click',function(){
+    document.body.classList.toggle('section')
+})
